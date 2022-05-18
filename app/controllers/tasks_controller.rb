c2bb202e1cf06af545fce939c58047e76f9cfeee
isabel bruda <ibrudap@gmail.com>
2022-05-18 11:35:15 +0200

    commiting beofre starting a new day

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: %i[show edit update destroy]
+
   def index
     @tasks = Task.all
   end
@@ -17,9 +19,30 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
 
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+
   def task_params
-    params.require(:task).permit(:title, :details)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
